a667cdb27e4004b6655edbcb4325329285c9fb8b
Thamaraiselvan Kodhandabani <thko2162@stu.sjcc.edu>
2026-04-22 18:39:44 -0700

    error correction

---
+++ src/app/order-placed/order-placed.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-order-placed',
+  imports: [RouterModule],
+  templateUrl: './order-placed.html',
+  styleUrl: './order-placed.css',
+})
+export class OrderPlaced {}
